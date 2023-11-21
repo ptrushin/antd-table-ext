8601b13b9b5c3fad9b2f3476c3cf1392c3f87212
ptrushin <ptrushin@custis.ru>
2023-11-21 14:11:28 +0300

    -

--- dist/index.js
+++ dist/index.js
@@ -1,6 +1,5 @@
 import Table from './Table';
-import { useReactRouterHistory } from './useReactRouterHistory';
 import ru_RU from './locale/ru_RU';
 import en_US from './locale/en_US';
-export { Table, useReactRouterHistory };
+export { Table };
 export { ru_RU, en_US };
